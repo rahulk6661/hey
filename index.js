c4e43be737f442d412abbadfebd0239572f567f2
rahulk6661 <133742767+rahulk6661@users.noreply.github.com>
2023-08-09 18:03:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
-var fs=require('fs');
 const http=require('http');
 
-const app=http.createServer();
-//old method here first we download data then show
-/*app.on("request",(req,res)=>
+const server=http.createServer((req,res)=>
 {
-fs.readFile('/home/rahul/Desktop/nodejs/firstpracticse/foldercreate/stream/input.txt','utf-8',(err,data)=>{
-if (err) return console.error(err);
-res.end(data);
-});
+    if(req.url==='/home')
+    {
+        
+        res.end("Welcome home")
+    }
+    else if(req.url==='/about')
+    {
+        
+        res.end("Welcome to About Us page");
+    }
+    else  if(req.url==='Welcome to my Node Js project')
+    {
+        console.log("you are at contact page");
+        res.end("you are at contact page");
+    }
+    
 });
 
-app.listen(3500,'127.0.0.1',()=>
+server.listen(4000,'127.0.0.1',()=>
 {
-console.log("server start");
-});*/
-//here we write bunch of code
-
-/*app.on("request",(req,res)=>{
-    const readstream=fs.createReadStream("/home/rahul/Desktop/nodejs/firstpracticse/foldercreate/stream/input.txt");
-    readstream.on("data",(chunkdata)=>
-    {
-        res.write(chunkdata);
-    })
-    readstream.on('end',()=>
-    {
-        res.end();
-    })
+    console.log("server is running")
 });
-app.listen(3500,'127.0.0.1',()=>
-{
-console.log("server start");
-});*/
-app.on("request",(req,res)=>{
-    const readstream=fs.createReadStream("/home/rahul/Desktop/nodejs/firstpracticse/foldercreate/stream/input.txt");//which file you want read
-    readstream.pipe(res);//where you want to write
-});
-app.listen(3500,'127.0.0.1',()=>
-{
-console.log("server start");
-})
